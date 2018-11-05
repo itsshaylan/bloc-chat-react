0899a72114cae5b3ce37272cb96313675f4871e0
Shayla <itsshaylan@gmail.com>
2018-11-05 14:36:32 -0600

    added firebase to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+
+<script src="https://www.gstatic.com/firebasejs/5.5.7/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDi8Le6Hc3F-Adn42lF5SP2wv-pcAoeKLs",
+    authDomain: "bloc-chat-react-c55d5.firebaseapp.com",
+    databaseURL: "https://bloc-chat-react-c55d5.firebaseio.com",
+    projectId: "bloc-chat-react-c55d5",
+    storageBucket: "bloc-chat-react-c55d5.appspot.com",
+    messagingSenderId: "133034382442"
+  };
+  firebase.initializeApp(config);
+</script>
 
 class App extends Component {
   render() {

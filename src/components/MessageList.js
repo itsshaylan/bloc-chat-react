2bf9a9514fea5ca816df1fe843887cfc0b40e8b5
Shayla <itsshaylan@gmail.com>
2018-11-09 02:01:15 -0600

    created messagelist component

---
+++ src/components/MessageList.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class MessageList extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+      messages: []
+    };
+
+    this.messagesRef = this.props.firebase.database().ref('messages');
+}
+
+ render() {
+    return (
+
+   )
+}
+
+export default MessageList;
